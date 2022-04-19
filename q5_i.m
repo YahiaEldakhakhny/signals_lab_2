c7e0853c583ac167d000212e53436ac30793530a
yahia <yahia4360673@gmail.com>
2022-04-19 11:50:49 +0200

    q5_i is working without brute force

--- q5_i.m
+++ q5_i.m
@@ -1,19 +1,27 @@
-x = [1 -2 4 6 -5 8 10];
-n = [-4 -3 -2 -1 0 1 2];
+x = [0 0 0 0 0 0 1 -2 4 6 -5 8 10 0 0 0 0 0 0 0 0];
+n = [-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10];
 
-n_total_start = min(n-2);
-n_total_end = max(n+4);
-n_total = [n_total_start:1:n_total_end];
+% x_s = x[n-s]
+function [x_s] = myshift(x,s)
+  x_s = x;
+  if s > 0
+    for i = 1:1:length(x)-s
+      x_s(i+s) = x(i);
+    endfor
+  else
+    for i = 1-s:1:length(x)
+      x_s(i+s) = x(i);
+    endfor
+  endif
 
-x_total = zeros(1, length(n_total));
-x_total(1:length(n)) = x_total(1:length(n)) + x.*3;
-x_total(end-length(n)+1:end) = x_total(end-length(n)+1:end) + x;
-x_total(3:2+length(x)) = x_total(3:2+length(x)) + x.*2;
+endfunction
 
-
+x_1 = myshift(x,-2).*3 + myshift(x,4) + x.*2;
 
 figure
 stem(n,x)
+grid minor
 
 figure
-stem(n_total,x_total)
+stem(n,x_1)
+grid minor
