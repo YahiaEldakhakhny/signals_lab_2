1195451097fe60ab49bb224e5dfd745bcc98acea
yahia <yahia4360673@gmail.com>
2022-04-19 12:47:49 +0200

    all the files are working elegantly

--- q5_ii.m
+++ q5_ii.m
@@ -1,30 +1,31 @@
-x = [1 -2 4 6 -5 8 10];
-n = [-4 -3 -2 -1 0 1 2];
+x = [0 0 0 0 0 0 1 -2 4 6 -5 8 10 0 0 0 0 0 0 0 0];
+n = [-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10];
 
-n_total_start = min(n-4);
-n_total_end = max(n+1);
-n_total = [n_total_start:1:n_total_end];
+% x_s = x[n-s]
+function [x_s] = myshift(x,s)
+  x_s = x;
+  if s > 0
+    for i = 1:1:length(x)-s
+      x_s(i+s) = x(i);
+    endfor
+  else
+    for i = 1-s:1:length(x)
+      x_s(i+s) = x(i);
+    endfor
+  endif
 
-x_total = zeros(1, length(n_total));
+endfunction
 
-x_4 = x_total;
-x_4(1:length(x)) = x;
+% generate x[-n]
+x_rev = x;
+x_rev(1:end) = x(end:-1:1);
 
-x_1 = x_total;
-x_1(end-length(x)+1:end) = x;
-
-x_total = x_total + (x_4.*x_1);
-
-x_2 = x(end:-1:1);
-x_2 = x_2.*x;
-xx_2 = x_total;
-xx_2(5:4+length(x_2)) = x_2;
-
-x_total = x_total + xx_2;
-
+x_2 = myshift(x,-4).*myshift(x,1) + myshift(x_rev,-2).*x
 
 figure
 stem(n,x)
+grid minor
 
 figure
-stem(n_total,x_total)
+stem(n, x_2)
+grid minor
